373336b0f6b9511c359cb4c9717d82a7d2691ad3
Sergii Podriezienko <podreenko1986@gmail.com>
2023-10-30 12:40:06 +0200

    add: array aspect
    add: array aspect

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ const aspectMoon = document.querySelector("#aspect-moon");
 const aspectMerk = document.querySelector("#aspect-merk");
 const aspectVen = document.querySelector("#aspect-ven");
 const aspectMars = document.querySelector("#aspect-mars");
+const aspectSaturn = document.querySelector("#aspect-saturn");
+const aspecJupiter = document.querySelector("#aspect-jupiter");
+const aspecUran = document.querySelector("#aspect-uran");
+const aspecNeptun = document.querySelector("#aspect-neptun");
+const aspecPluton = document.querySelector("#aspect-pluton");
 
 const aspectDegrMoon = document.querySelector("#aspect-degr-moon");
 const aspectMinMoon = document.querySelector("#aspect-min-moon");
@@ -22,6 +27,21 @@ const aspectMinVen = document.querySelector("#aspect-min-ven");
 
 const aspectDegrMars = document.querySelector("#aspect-degr-mars");
 const aspectMinMars = document.querySelector("#aspect-min-mars");
+
+const aspectDegrSaturn = document.querySelector("#aspect-degr-saturn");
+const aspectMinSaturn = document.querySelector("#aspect-min-saturn");
+
+const aspectDegrJupiter = document.querySelector("#aspect-degr-jupiter");
+const aspectMinJupiter = document.querySelector("#aspect-min-jupiter");
+
+const aspectDegrUran = document.querySelector("#aspect-degr-uran");
+const aspectMinUran = document.querySelector("#aspect-min-uran");
+
+const aspectDegrNeptun = document.querySelector("#aspect-degr-neptun");
+const aspectMinNeptun = document.querySelector("#aspect-min-neptun");
+
+const aspectDegrPluton = document.querySelector("#aspect-degr-pluton");
+const aspectMinPluton = document.querySelector("#aspect-min-pluton");
 
 function getDegr(znZodiak) {
   if (znZodiak === "Овен") {
@@ -116,6 +136,63 @@ function addElemenArray(datePlanet, planet) {
     };
     datePlanet.push(objMoon);
   }
+  if (planet === "Сатурн") {
+    const valueDegrSaturn = parseInt(aspectDegrSaturn.value);
+    const valueMinSaturn = parseInt(aspectMinSaturn.value);
+    const planetSaturn =
+      aspectSaturn.options[aspectSaturn.value - 1].textContent;
+    const objMoon = {
+      planet: "Сатурн",
+      min: valueMinSaturn,
+      degr: getDegr(planetSaturn) + valueDegrSaturn,
+    };
+    datePlanet.push(objMoon);
+  }
+  if (planet === "Юпітер") {
+    const valueDegrJupiter = parseInt(aspectDegrJupiter.value);
+    const valueMinJupiter = parseInt(aspectMinJupiter.value);
+    const planetJupiter =
+      aspecJupiter.options[aspecJupiter.value - 1].textContent;
+    const objMoon = {
+      planet: "Юпітер",
+      min: valueMinJupiter,
+      degr: getDegr(planetJupiter) + valueDegrJupiter,
+    };
+    datePlanet.push(objMoon);
+  }
+  if (planet === "Уран") {
+    const valueDegrUran = parseInt(aspectDegrUran.value);
+    const valueMinUran = parseInt(aspectMinUran.value);
+    const planetUran = aspecUran.options[aspecUran.value - 1].textContent;
+    const objMoon = {
+      planet: "Уран",
+      min: valueMinUran,
+      degr: getDegr(planetUran) + valueDegrUran,
+    };
+    datePlanet.push(objMoon);
+  }
+  if (planet === "Нептун") {
+    const valueDegrNeptun = parseInt(aspectDegrNeptun.value);
+    const valueMinNeptun = parseInt(aspectMinNeptun.value);
+    const planetNeptun = aspecNeptun.options[aspecNeptun.value - 1].textContent;
+    const objMoon = {
+      planet: "Нептун",
+      min: valueMinNeptun,
+      degr: getDegr(planetNeptun) + valueDegrNeptun,
+    };
+    datePlanet.push(objMoon);
+  }
+  if (planet === "Плутон") {
+    const valueDegrPluton = parseInt(aspectDegrPluton.value);
+    const valueMinPluton = parseInt(aspectMinPluton.value);
+    const planetPluton = aspecPluton.options[aspecPluton.value - 1].textContent;
+    const objMoon = {
+      planet: "Плутон",
+      min: valueMinPluton,
+      degr: getDegr(planetPluton) + valueDegrPluton,
+    };
+    datePlanet.push(objMoon);
+  }
 }
 
 function onClickAspectCalc() {
@@ -125,6 +202,11 @@ function onClickAspectCalc() {
   addElemenArray(datePlanet, "Меркурій");
   addElemenArray(datePlanet, "Венера");
   addElemenArray(datePlanet, "Марс");
+  addElemenArray(datePlanet, "Сатурн");
+  addElemenArray(datePlanet, "Юпітер");
+  addElemenArray(datePlanet, "Уран");
+  addElemenArray(datePlanet, "Нептун");
+  addElemenArray(datePlanet, "Плутон");
 
   for (let i = 0; i < datePlanet.length; i += 1) {
     console.log(datePlanet[i]);
