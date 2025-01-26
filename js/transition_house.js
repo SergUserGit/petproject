885f53f89644e8056dd55e0ca312e1f5d1f8d26e
Sergii Podriezienko <podreenko1986@gmail.com>
2025-01-26 11:33:45 +0200

    add: planet values
    add: planet values

--- js/transition_house.js
+++ js/transition_house.js
@@ -1,5 +1,16 @@
 const calc_button = document.querySelector(".transit_button_calc");
 
+const planetSunInput = document.querySelector("#aspect-sun");
+const planetMonInput = document.querySelector("#aspect-moon");
+const planetMerkInput = document.querySelector("#aspect-merk");
+const planetVenerInput = document.querySelector("#aspect-vener");
+const planetMarsInput = document.querySelector("#aspect-mars");
+const planetUpiterInput = document.querySelector("#aspect-yupiter");
+const planetSaturnInput = document.querySelector("#aspect-saturn");
+const planetUranInput = document.querySelector("#aspect-uran");
+const planetNeptnunInput = document.querySelector("#aspect-neptun");
+const planetPlutonInput = document.querySelector("#aspect-pluton");
+
 calc_button.addEventListener("click", onClickTransHouseButton);
 
 function addHouseInArray(house, znack, arrayOnHouses) {
@@ -37,18 +48,66 @@ function addPlanetArray(planet, znZodiak, arrayPlanet) {
   arrayPlanet.push(objPlanet);
 }
 
+function getObjectOfValuePlanet() {
+  let planetSunValue =
+    planetSunInput.options[planetSunInput.value - 1].textContent;
+
+  let planetMonValue =
+    planetMonInput.options[planetMonInput.value - 1].textContent;
+
+  let planetMerkValue =
+    planetMerkInput.options[planetMerkInput.value - 1].textContent;
+
+  let planetVenerValue =
+    planetVenerInput.options[planetVenerInput.value - 1].textContent;
+
+  let planetMarsValue =
+    planetMarsInput.options[planetMarsInput.value - 1].textContent;
+
+  let planetUpiterValue =
+    planetUpiterInput.options[planetUpiterInput.value - 1].textContent;
+
+  let planetSaturnValue =
+    planetSaturnInput.options[planetSaturnInput.value - 1].textContent;
+
+  let planetUranValue =
+    planetUranInput.options[planetUranInput.value - 1].textContent;
+
+  let planetNeptunValue =
+    planetNeptnunInput.options[planetNeptnunInput.value - 1].textContent;
+
+  let planetPlunonValue =
+    planetPlutonInput.options[planetPlutonInput.value - 1].textContent;
+
+  const objValues = {
+    planetSunValue,
+    planetMonValue,
+    planetMerkValue,
+    planetVenerValue,
+    planetMarsValue,
+    planetUpiterValue,
+    planetSaturnValue,
+    planetUranValue,
+    planetNeptunValue,
+    planetPlunonValue,
+  };
+  return objValues;
+}
+
 function getArrayPlanetInZnZod() {
+  const objOfValues = getObjectOfValuePlanet();
+
   let arrayPlanet = [];
-  addPlanetArray("Сонце", "Водолій", arrayPlanet);
-  addPlanetArray("Місяць", "Стрілець", arrayPlanet);
-  addPlanetArray("Меркурій", "Козеріг", arrayPlanet);
-  addPlanetArray("Венера", "Риби", arrayPlanet);
-  addPlanetArray("Марс", "Рак", arrayPlanet);
-  addPlanetArray("Юпітер", "Близнюки", arrayPlanet);
-  addPlanetArray("Сатурн", "Риби", arrayPlanet);
-  addPlanetArray("Уран", "Тілець", arrayPlanet);
-  addPlanetArray("Нептун", "Риби", arrayPlanet);
-  addPlanetArray("Плутон", "Водолій", arrayPlanet);
+  addPlanetArray("Сонце", objOfValues.planetSunValue, arrayPlanet);
+  addPlanetArray("Місяць", objOfValues.planetMonValue, arrayPlanet);
+  addPlanetArray("Меркурій", objOfValues.planetMerkValue, arrayPlanet);
+  addPlanetArray("Венера", objOfValues.planetVenerValue, arrayPlanet);
+  addPlanetArray("Марс", objOfValues.planetMarsValue, arrayPlanet);
+  addPlanetArray("Юпітер", objOfValues.planetUpiterValue, arrayPlanet);
+  addPlanetArray("Сатурн", objOfValues.planetSaturnValue, arrayPlanet);
+  addPlanetArray("Уран", objOfValues.planetUranValue, arrayPlanet);
+  addPlanetArray("Нептун", objOfValues.planetNeptunValue, arrayPlanet);
+  addPlanetArray("Плутон", objOfValues.planetPlunonValue, arrayPlanet);
   return arrayPlanet;
 }
 
